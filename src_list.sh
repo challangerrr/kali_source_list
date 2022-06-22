e6263168d28881dbe4f8dad1a4c21f6135a63fc7
peace_mason <59176416+challangerrr@users.noreply.github.com>
2022-06-22 12:47:59 +0530

    Add files via upload

---
+++ src_list.sh
@@ -0,0 +1,6 @@
+ifconfig
+echo ' # See https://www.kali.org/docs/general-use/kali-linux-sources-list-repositories/ \ndeb http://ftp.free.fr/pub/kali kali-rolling main contrib non-free \n# Additional line for source packages \ndeb-src http://ftp.free.fr/pub/kali kali-rolling main contrib non-free' > /etc/apt/source.list
+
+cat /etc/apt/source.list
+
+apt update
